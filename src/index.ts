4d49d6c34d7f0c4282fd05fc628457afcacc5088
Daniel Nixon <dan.nixon@gmail.com>
2019-09-13 18:51:13 +1000

    Add ReadonlyDate

--- src/index.ts
+++ src/index.ts
@@ -45,3 +45,91 @@ export type ReadonlyURL = {
 
 export const ReadonlyURL = (url: string, base?: string | URL): ReadonlyURL =>
   new URL(url, base);
+
+export type ReadonlyDate = {
+  /** Returns a string representation of a date. The format of the string depends on the locale. */
+  readonly toString: () => string;
+  /** Returns a date as a string value. */
+  readonly toDateString: () => string;
+  /** Returns a time as a string value. */
+  readonly toTimeString: () => string;
+  /** Returns a value as a string value appropriate to the host environment's current locale. */
+  readonly toLocaleString: () => string;
+  /** Returns a date as a string value appropriate to the host environment's current locale. */
+  readonly toLocaleDateString: () => string;
+  /** Returns a time as a string value appropriate to the host environment's current locale. */
+  readonly toLocaleTimeString: () => string;
+  /** Returns the stored time value in milliseconds since midnight, January 1, 1970 UTC. */
+  readonly valueOf: () => number;
+  /** Gets the time value in milliseconds. */
+  readonly getTime: () => number;
+  /** Gets the year, using local time. */
+  readonly getFullYear: () => number;
+  /** Gets the year using Universal Coordinated Time (UTC). */
+  readonly getUTCFullYear: () => number;
+  /** Gets the month, using local time. */
+  readonly getMonth: () => number;
+  /** Gets the month of a Date object using Universal Coordinated Time (UTC). */
+  readonly getUTCMonth: () => number;
+  /** Gets the day-of-the-month, using local time. */
+  readonly getDate: () => number;
+  /** Gets the day-of-the-month, using Universal Coordinated Time (UTC). */
+  readonly getUTCDate: () => number;
+  /** Gets the day of the week, using local time. */
+  readonly getDay: () => number;
+  /** Gets the day of the week using Universal Coordinated Time (UTC). */
+  readonly getUTCDay: () => number;
+  /** Gets the hours in a date, using local time. */
+  readonly getHours: () => number;
+  /** Gets the hours value in a Date object using Universal Coordinated Time (UTC). */
+  readonly getUTCHours: () => number;
+  /** Gets the minutes of a Date object, using local time. */
+  readonly getMinutes: () => number;
+  /** Gets the minutes of a Date object using Universal Coordinated Time (UTC). */
+  readonly getUTCMinutes: () => number;
+  /** Gets the seconds of a Date object, using local time. */
+  readonly getSeconds: () => number;
+  /** Gets the seconds of a Date object using Universal Coordinated Time (UTC). */
+  readonly getUTCSeconds: () => number;
+  /** Gets the milliseconds of a Date, using local time. */
+  readonly getMilliseconds: () => number;
+  /** Gets the milliseconds of a Date object using Universal Coordinated Time (UTC). */
+  readonly getUTCMilliseconds: () => number;
+  /** Gets the difference in minutes between the time on the local computer and Universal Coordinated Time (UTC). */
+  readonly getTimezoneOffset: () => number;
+
+  /** Returns a date converted to a string using Universal Coordinated Time (UTC). */
+  readonly toUTCString: () => string;
+  /** Returns a date as a string value in ISO format. */
+  readonly toISOString: () => string;
+
+  // tslint:disable: no-any
+  /** Used by the JSON.stringify method to enable the transformation of an object's data for JavaScript Object Notation (JSON) serialization. */
+  readonly toJSON: (key?: any) => string;
+  // tslint:enable: no-any
+
+  // tslint:disable: no-method-signature
+  /**
+   * Converts a Date object to a string.
+   */
+  [Symbol.toPrimitive](hint: "default" | "string"): string;
+  /**
+   * Converts a Date object to a number.
+   */
+  [Symbol.toPrimitive](hint: "number"): number;
+  /**
+   * Converts a Date object to a string or number.
+   *
+   * @param hint The strings "number", "string", or "default" to specify what primitive to return.
+   *
+   * @throws {TypeError} If 'hint' was given something other than "number", "string", or "default".
+   * @returns A number if 'hint' was "number", a string if 'hint' was "string" or "default".
+   */
+  [Symbol.toPrimitive](hint: string): string | number;
+  // tslint:enable: no-method-signature
+};
+
+// tslint:disable-next-line: max-union-size
+export const ReadonlyDate = (
+  value: number | string | Date | ReadonlyDate
+): ReadonlyDate => new Date(value as number | string | Date);
