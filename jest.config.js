f61c9cb45b3f8b37f29aa3eefd36e107b5dea291
Daniel Nixon <dan.nixon@gmail.com>
2020-06-01 09:31:05 +1000

    Enforce coverage thresholds

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,13 @@ module.exports = {
   "transform": {
     "^.+\\.tsx?$": "ts-jest"
   },
-  "collectCoverage": true
+  "collectCoverage": true,
+  "coverageThreshold": {
+    "global": {
+      "branches": 100,
+      "functions": 14.29,
+      "lines": 57.14,
+      "statements": 57.14
+    }
+  },
 }
